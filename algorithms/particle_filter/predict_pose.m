75c7c59395fcc8a06255661354d0c2296bdb13b2
Tomáš Linek <xlinek03@vut.cz>
2026-04-07 19:26:50 +0200

    week 4 task 1

--- algorithms/particle_filter/predict_pose.m
+++ algorithms/particle_filter/predict_pose.m
@@ -1,7 +1,32 @@
 function [new_pose] = predict_pose(old_pose, motion_vector, read_only_vars)
 %PREDICT_POSE Summary of this function goes here
 
-new_pose = old_pose;
+x_old = old_pose(1);
+y_old = old_pose(2);
+theta_old = old_pose(3);
+
+v_r = motion_vector(1);
+v_l = motion_vector(2);
+
+v = (v_r + v_l) / 2;
+omega = (v_r - v_l) / read_only_vars.agent_drive.interwheel_dist;
+
+
+x_new = x_old - (v / omega) * sin(theta_old) + (v / omega) * sin(theta_old + omega * read_only_vars.sampling_period);
+y_new = y_old + (v / omega) * cos(theta_old) - (v / omega) * cos(theta_old + omega * read_only_vars.sampling_period);
+theta_new = theta_old + omega * read_only_vars.sampling_period;
+
+
+sigma_pose_noise = 0.03
+sigma_theta_noise = 0.08
+
+x_new = x_new + randn() * sigma_pose_noise;
+y_new = y_new + randn() * sigma_pose_noise;
+theta_new = theta_new + randn() * sigma_theta_noise;
+
+new_pose = [x_new;
+            y_new;
+            theta_new];
 
 end
 
